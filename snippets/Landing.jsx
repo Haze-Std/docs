56485e02d3e46bf8ae3b3132b067ba02435ff863
Zach Simms <59024395+ZachSimms@users.noreply.github.com>
2026-01-19 12:04:57 -0600

    Landing Page

--- snippets/Landing.jsx
+++ snippets/Landing.jsx
@@ -1,6 +1,6 @@
 export const Landing = () => {
     return (
-        <div>
+        <div className="flex w-full">
             <h1>Hello World</h1>
             <p>Learn how to get started with the Claude Developer Platform and Claude Code</p>
         </div>
