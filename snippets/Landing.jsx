b11baebaf69c9783024c7530bb2a7b5514eb59ed
Zach Simms <59024395+ZachSimms@users.noreply.github.com>
2026-01-19 11:57:49 -0600

    Style Landing

---
+++ snippets/Landing.jsx
@@ -0,0 +1,8 @@
+export const Landing = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Learn how to get started with the Claude Developer Platform and Claude Code</p>
+        </div>
+    )
+}
